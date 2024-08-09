001863eab41ae004d9ce511803e1ac07415baf4a
lucas-perata <contacto.perata@gmail.com>
2024-08-09 14:36:55 -0300

    progress cv

--- src/components/extras/CardSkills.tsx
+++ src/components/extras/CardSkills.tsx
@@ -4,7 +4,7 @@ function CardSkills({ link }) {
   return (
     <div className="transition ease-in-out hover:-translate-y-1 hover:scale-100 duration-200 card w-52 bg-gradient-to-r from-sky-700 via-purple-500 to-sky-900 shadow-sm hover:shadow-xl">
       <div className="card-body items-center ">
-        <img width={100} src={link} />
+        <img alt="habilidades-icono" width={100} src={link} />
       </div>
     </div>
   )
--- src/components/extras/ProjectCard.tsx
+++ src/components/extras/ProjectCard.tsx
@@ -3,19 +3,19 @@ import { FaGithub } from "react-icons/fa";
 
 function ProjectCard({ side, title }) {
   return (
-    <div className={'w-[60vw] gap-5 flex justify-between  ' + (side == true ? "flex-row-reverse" : "flex-row")}>
-      <div className={'gap-5 self-center  flex flex-col ' + (side == true ? "text-right" : "text-left")}>
+    <div className={'w-[60vw] gap-5 flex justify-between  ' + (side === true ? "flex-row-reverse" : "flex-row")}>
+      <div className={'gap-5 self-center  flex flex-col ' + (side === true ? "text-right" : "text-left")}>
         <h3 className='text-2xl font-bold'>{title}</h3>
         <p className='max-w-[40vw] bg-indigo-800 p-4 rounded-lg shadow-lg'>Más descripciones de los proyectos largas descripciones de los proyectos Descripción descripción descripción de palabras Descripción descripción descripción de palabras</p>
         <p>.NET 8.0 - PostgreSQL - Next.JS</p>
-        <FaGithub size={24} className={'' + (side == true ? "self-end" : "self-start")} />
+        <FaGithub size={24} className={'' + (side === true ? "self-end" : "self-start")} />
       </div>
       <div className="mockup-browser bg-base-300 border">
         <div className="mockup-browser-toolbar">
           <div className="input">https://daisyui.com</div>
         </div>
         <div className="bg-base-200 flex justify-center px-4 p-1">
-          <img src='homer.gif' className="rounded-xl" />
+          <img alt="proyecto-img" src='homer.gif' className="rounded-xl" />
         </div>
       </div>
     </div>
