846f024e03b0bf510829c7228d2b9611033438c8
lucas-perata <contacto.perata@gmail.com>
2024-08-14 13:18:56 -0300

    Added basics of markdown blog

---
+++ public/blog/test.md
@@ -0,0 +1,15 @@
+# Test post 
+
+This is a post testing for markdown. 
+
+## This is second title 
+Markdown: 
+- One 
+- Two 
+- **three** 
+
+### Third title 
+Without anything else Without anything else Without anything else
+Without anything elseWithout anything elseWithout anything elseWithout anything elseWithout anything else
+Without anything elseWithout anything elseWithout anything elseWithout anything else
+pWithout anything elseWithout anything elseWithout anything elseWithout anything else
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,12 +1,26 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import "../components/landing/landing.css"
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase-config";
-import Markdown from "react-markdown";
+import Markdown from "markdown-to-jsx";
 
 function Blog() {
+  const file_name = "test.md";
+  const [post, setPost] = useState('');
+  useEffect(() => {
+    import(`../../public/blog/${file_name}`)
+      .then(res => {
+        fetch(res.default)
+          .then(res => res.text())
+          .then(res => setPost(res));
+      })
+      .catch(err => console.log(err));
+  })
   return (
-    <div className="hero-b min-h-[60vh]">
+    <div className="hero-b h-fit prose lg:prose-lg flex justify-center min-w-[100%]">
+      <div className="flex h-fit border rounded-xl shadow-xl p-10 bg-indigo-900 text-white w-1/2 ">
+        <Markdown>
+          {post}
+        </Markdown>
+      </div>
     </div>
   )
 }
